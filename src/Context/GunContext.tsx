66d0298b21f2f7dd3ec5147d55f2f84030edb347
Bruno Camarneiro <bruno.camarneiro@carbonify.de>
2024-09-09 12:18:31 +0200

    feat: :sparkles: Add debug option

--- src/Context/GunContext.tsx
+++ src/Context/GunContext.tsx
@@ -6,16 +6,27 @@ const GunContext = createContext<IGunInstance | null>(null);
 interface GunProviderProps extends PropsWithChildren {
   gun?: IGunInstance;
   options?: GunOptions;
+  debug?: boolean;
 }
 
-export const GunProvider = ({ children, options, gun }: GunProviderProps) => {
+export const GunProvider = ({ children, options, gun, debug }: GunProviderProps) => {
+  if (debug) {
+    console.debug('GunProvider', options, gun);
+  }
+
   const gunInstance = useMemo(() => gun ? gun : Gun(options), [options, gun]);
 
   if (!gunInstance) {
+    console.error('GunProvider', 'gunInstance is null');
     return null;
+  }
+
+  if (debug) {
+    console.debug('GunProvider', gunInstance);
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    (window as any).gun = gunInstance;
   }
 
   return <GunContext.Provider value={gunInstance}>{children}</GunContext.Provider>;
 };
-
 export default GunContext;
